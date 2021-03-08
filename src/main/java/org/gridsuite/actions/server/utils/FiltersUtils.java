1174cca88404ccfde66d5a39213cdbed657ced46
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2021-03-08 13:25:19 +0100

    Fix author and license
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- src/main/java/org/gridsuite/actions/server/utils/FiltersUtils.java
+++ src/main/java/org/gridsuite/actions/server/utils/FiltersUtils.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.actions.server.utils;
 
 import com.powsybl.iidm.network.Connectable;
@@ -8,6 +14,9 @@ import org.springframework.util.PathMatcher;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * @author Franck Lecuyer <franck.lecuyer at rte-france.com>
+ */
 public final class FiltersUtils {
     private static final PathMatcher ANT_MATCHER = new AntPathMatcher("\0");
 
@@ -21,11 +30,7 @@ public final class FiltersUtils {
 
     public static boolean matchName(String filterName, Identifiable<?> equipment) {
         Optional<String> name = equipment.getOptionalName();
-        if (name.isPresent()) {
-            return ANT_MATCHER.match(filterName, name.get());
-        } else {
-            return false;
-        }
+        return name.filter(s -> ANT_MATCHER.match(filterName, s)).isPresent();
     }
 
     public static boolean isLocatedIn(List<String> filterCountries, Connectable<?> equipment) {
