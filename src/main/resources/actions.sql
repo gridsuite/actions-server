252d1dc97ee6c1fb14d3a4fa358f4caa6c16329b
jacqbors <jacques.borsenberger@rte-france.com>
2021-06-29 16:17:36 +0200

    use UUID to identify Contingency lists
    Signed-off-by: jacqbors <jacques.borsenberger@rte-france.com>

--- src/main/resources/actions.sql
+++ src/main/resources/actions.sql
@@ -1,27 +1,62 @@
 
     create table filters_contingency_list (
-       name varchar(255) not null,
+       idList uuid not null,
         equipmentId varchar(255),
         equipmentName varchar(255),
         equipmentType varchar(255),
+        name varchar(255),
         nominalVoltage float8,
         nominalVoltageOperator varchar(255),
-        primary key (name)
+        primary key (idList)
     );
 
     create table FiltersContingencyListEntity_countries (
-       FiltersContingencyListEntity_name varchar(255) not null,
+       FiltersContingencyListEntity_idList uuid not null,
         country varchar(255)
     );
 
     create table script_contingency_list (
-       name varchar(255) not null,
+       id uuid not null,
+        name varchar(255),
         script TEXT,
-        primary key (name)
+        primary key (id)
     );
-create index filtersContingencyListEntity_countries_idx on FiltersContingencyListEntity_countries (FiltersContingencyListEntity_name);
+create index script_contingency_list_name_idx on filters_contingency_list (name);
+create index filtersContingencyListEntity_countries_idx on FiltersContingencyListEntity_countries (FiltersContingencyListEntity_idList);
+create index script_contingency_list_name_idx on script_contingency_list (name);
 
     alter table if exists FiltersContingencyListEntity_countries 
        add constraint filtersContingencyListEntity_countries_fk 
-       foreign key (FiltersContingencyListEntity_name) 
+       foreign key (FiltersContingencyListEntity_idList) 
+       references filters_contingency_list;
+
+    create table filters_contingency_list (
+       idList uuid not null,
+        equipmentId varchar(255),
+        equipmentName varchar(255),
+        equipmentType varchar(255),
+        name varchar(255),
+        nominalVoltage float8,
+        nominalVoltageOperator varchar(255),
+        primary key (idList)
+    );
+
+    create table FiltersContingencyListEntity_countries (
+       FiltersContingencyListEntity_idList uuid not null,
+        country varchar(255)
+    );
+
+    create table script_contingency_list (
+       id uuid not null,
+        name varchar(255),
+        script TEXT,
+        primary key (id)
+    );
+create index script_contingency_list_name_idx on filters_contingency_list (name);
+create index filtersContingencyListEntity_countries_idx on FiltersContingencyListEntity_countries (FiltersContingencyListEntity_idList);
+create index script_contingency_list_name_idx on script_contingency_list (name);
+
+    alter table if exists FiltersContingencyListEntity_countries 
+       add constraint filtersContingencyListEntity_countries_fk 
+       foreign key (FiltersContingencyListEntity_idList) 
        references filters_contingency_list;
