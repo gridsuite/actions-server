b2b2d0f598d8aa081a770dc56d12d70d7b777f7a
jacqbors <jacques.borsenberger@rte-france.com>
2021-07-01 17:09:55 +0200

    remarks
    Signed-off-by: jacqbors <jacques.borsenberger@rte-france.com>

--- src/main/resources/actions.sql
+++ src/main/resources/actions.sql
@@ -1,62 +1,37 @@
 
     create table filters_contingency_list (
-       idList uuid not null,
+       id uuid not null,
+        creationDate timestamp,
+        description varchar(255),
+        modificationDate timestamp,
+        name varchar(255),
         equipmentId varchar(255),
         equipmentName varchar(255),
         equipmentType varchar(255),
-        name varchar(255),
         nominalVoltage float8,
         nominalVoltageOperator varchar(255),
-        primary key (idList)
+        primary key (id)
     );
 
     create table FiltersContingencyListEntity_countries (
-       FiltersContingencyListEntity_idList uuid not null,
+       FiltersContingencyListEntity_id uuid not null,
         country varchar(255)
     );
 
     create table script_contingency_list (
        id uuid not null,
+        creationDate timestamp,
+        description varchar(255),
+        modificationDate timestamp,
         name varchar(255),
         script TEXT,
         primary key (id)
     );
-create index script_contingency_list_name_idx on filters_contingency_list (name);
-create index filtersContingencyListEntity_countries_idx on FiltersContingencyListEntity_countries (FiltersContingencyListEntity_idList);
+create index filter_contingency_list_name_idx on filters_contingency_list (name);
+create index filtersContingencyListEntity_countries_idx on FiltersContingencyListEntity_countries (FiltersContingencyListEntity_id);
 create index script_contingency_list_name_idx on script_contingency_list (name);
 
     alter table if exists FiltersContingencyListEntity_countries 
        add constraint filtersContingencyListEntity_countries_fk 
-       foreign key (FiltersContingencyListEntity_idList) 
-       references filters_contingency_list;
-
-    create table filters_contingency_list (
-       idList uuid not null,
-        equipmentId varchar(255),
-        equipmentName varchar(255),
-        equipmentType varchar(255),
-        name varchar(255),
-        nominalVoltage float8,
-        nominalVoltageOperator varchar(255),
-        primary key (idList)
-    );
-
-    create table FiltersContingencyListEntity_countries (
-       FiltersContingencyListEntity_idList uuid not null,
-        country varchar(255)
-    );
-
-    create table script_contingency_list (
-       id uuid not null,
-        name varchar(255),
-        script TEXT,
-        primary key (id)
-    );
-create index script_contingency_list_name_idx on filters_contingency_list (name);
-create index filtersContingencyListEntity_countries_idx on FiltersContingencyListEntity_countries (FiltersContingencyListEntity_idList);
-create index script_contingency_list_name_idx on script_contingency_list (name);
-
-    alter table if exists FiltersContingencyListEntity_countries 
-       add constraint filtersContingencyListEntity_countries_fk 
-       foreign key (FiltersContingencyListEntity_idList) 
+       foreign key (FiltersContingencyListEntity_id) 
        references filters_contingency_list;
