a79b10a8bc986c5916c5851fd435f6f8245b3b0d
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2022-06-27 10:05:46 +0200

    Migrate to PowSyBl dependencies 1.2.0 (#49)
    * Migrate to PowSyBl dependencies 1.2.0
    * Add commons properties init
    
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- src/main/java/org/gridsuite/actions/server/ActionsApplication.java
+++ src/main/java/org/gridsuite/actions/server/ActionsApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import com.powsybl.ws.commons.Utils;
 
 /**
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
@@ -22,6 +23,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class ActionsApplication {
 
     public static void main(String[] args) {
+        Utils.initProperties();
         SpringApplication.run(ActionsApplication.class, args);
     }
 
