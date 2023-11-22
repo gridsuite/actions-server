9ad721e90264747f72d156ebc8aad25bf30ef36a
sBouzols <sylvain.bouzols@gmail.com>
2023-11-22 16:46:29 +0100

    update to gridsuite dependencies 28
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/main/java/org/gridsuite/actions/server/ActionsApplication.java
+++ src/main/java/org/gridsuite/actions/server/ActionsApplication.java
@@ -12,7 +12,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import com.powsybl.ws.commons.Utils;
 
 /**
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
@@ -23,7 +22,6 @@ import com.powsybl.ws.commons.Utils;
 public class ActionsApplication {
 
     public static void main(String[] args) {
-        Utils.initProperties();
         SpringApplication.run(ActionsApplication.class, args);
     }
 
