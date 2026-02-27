b615376f4e9ab09359193af58bb8dd2f7c5798f1
klesaulnier <42617371+klesaulnier@users.noreply.github.com>
2026-02-27 14:12:28 +0100

    Extract some code to new lib actions (#152)
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>
    Co-authored-by: Antoine Bouhours <antoine.bouhours@rte-france.com>

--- src/main/java/org/gridsuite/actions/server/service/FilterService.java
+++ src/main/java/org/gridsuite/actions/server/service/FilterService.java
@@ -8,10 +8,8 @@
 package org.gridsuite.actions.server.service;
 
 import lombok.Getter;
-import org.gridsuite.actions.server.dto.FilterAttributes;
-import org.gridsuite.actions.server.dto.FilterBasedContingencyList;
-import org.gridsuite.filter.identifierlistfilter.FilteredIdentifiables;
-import org.gridsuite.filter.identifierlistfilter.IdentifiableAttributes;
+import org.gridsuite.actions.dto.FilterAttributes;
+import org.gridsuite.filter.AbstractFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -23,13 +21,13 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class FilterService {
-    public static final String FILTER_END_POINT_EVALUATE_IDS = "/filters/evaluate/identifiables";
     public static final String FILTER_END_POINT_INFOS_IDS = "/filters/infos";
+    public static final String FILTER_END_POINT_METADATA = "/filters/metadata";
     public static final String DELIMITER = "/";
     public static final String FILTER_API_VERSION = "v1";
 
@@ -42,24 +40,6 @@ public class FilterService {
                          RestTemplateBuilder restTemplateBuilder) {
         this.baseUri = baseUri;
         this.restTemplate = restTemplateBuilder.build();
-    }
-
-    public List<IdentifiableAttributes> evaluateFilters(UUID networkUuid, String variantUuid, FilterBasedContingencyList filterBasedContingencyList) {
-        Objects.requireNonNull(networkUuid);
-        Objects.requireNonNull(filterBasedContingencyList);
-        String endPointUrl = getBaseUri() + DELIMITER + FILTER_API_VERSION + FILTER_END_POINT_EVALUATE_IDS;
-
-        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromHttpUrl(endPointUrl);
-        uriComponentsBuilder.queryParam("networkUuid", networkUuid);
-        uriComponentsBuilder.queryParam("variantUuid", variantUuid);
-        var uriComponent = uriComponentsBuilder.buildAndExpand();
-
-        var headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<FilterBasedContingencyList> request = new HttpEntity<>(filterBasedContingencyList, headers);
-
-        ResponseEntity<FilteredIdentifiables> response = restTemplate.postForEntity(uriComponent.toUriString(), request, FilteredIdentifiables.class);
-        return response.getBody() != null ? response.getBody().equipmentIds() : List.of();
     }
 
     public List<FilterAttributes> getFiltersAttributes(List<UUID> filtersUuid, String userId) {
@@ -75,8 +55,17 @@ public class FilterService {
         headers.set("userId", userId);
 
         HttpEntity<String> entity = new HttpEntity<>(headers);
-        ResponseEntity<List<FilterAttributes> >response = restTemplate.exchange(uriComponent.toUriString(),
+        ResponseEntity<List<FilterAttributes>> response = restTemplate.exchange(uriComponent.toUriString(),
             HttpMethod.GET, entity, new ParameterizedTypeReference<>() { });
+        return response.getBody() != null ? response.getBody() : new ArrayList<>();
+    }
+
+    public List<AbstractFilter> getFilters(List<UUID> filtersUuids) {
+        var ids = !filtersUuids.isEmpty() ? "?ids=" + filtersUuids.stream().map(UUID::toString).collect(Collectors.joining(",")) : "";
+        String path = UriComponentsBuilder.fromPath(DELIMITER + FILTER_API_VERSION + FILTER_END_POINT_METADATA + ids)
+            .buildAndExpand()
+            .toUriString();
+        ResponseEntity<List<AbstractFilter>> response = restTemplate.exchange(getBaseUri() + path, HttpMethod.GET, null, new ParameterizedTypeReference<>() { });
         return response.getBody() != null ? response.getBody() : new ArrayList<>();
     }
 }
