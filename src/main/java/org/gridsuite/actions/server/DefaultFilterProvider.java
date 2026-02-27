a647a424c249d6f6cb31a44e8eebc49b8f837205
LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>
2026-02-27 10:00:29 +0100

    add test coverage
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>

--- src/main/java/org/gridsuite/actions/server/DefaultFilterProvider.java
+++ src/main/java/org/gridsuite/actions/server/DefaultFilterProvider.java
@@ -4,11 +4,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package org.gridsuite.actions.server.service;
+package org.gridsuite.actions.server;
 
 import org.gridsuite.actions.FilterProvider;
+import org.gridsuite.actions.server.service.FilterService;
 import org.gridsuite.filter.AbstractFilter;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.UUID;
@@ -16,7 +17,7 @@ import java.util.UUID;
 /**
  * @author Kevin Le Saulnier <kevin.le-saulnier at rte-france.com>
  */
-@Service
+@Component
 public class DefaultFilterProvider implements FilterProvider {
 
     private final FilterService filterService;
